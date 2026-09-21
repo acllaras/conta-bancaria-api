a83546e93af69cf5e091afa2a7131037598daff6
Ana Clara <anacpeixoto104@gmail.com>
2026-09-21 13:18:37 -0300

    docs: adiciona documentacao Swagger da API

---
+++ src/main/java/com/pactomais/contabancariaapi/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.pactomais.contabancariaapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Conta Bancária API")
+                        .version("1.0")
+                        .description(
+                                "API RESTful para gerenciamento de correntistas, contas bancárias e transações."
+                        ));
+    }
+}
